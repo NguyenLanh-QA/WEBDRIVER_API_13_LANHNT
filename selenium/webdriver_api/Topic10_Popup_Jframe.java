3ad03ad4fb7a8e03f06eb8ec9749f7c7a1af7787
nguyenlanhvfu@gmail.com <nguye@DESKTOP-0F1HJ6C>
2020-02-10 18:12:06 +0700

    11

--- selenium/webdriver_api/Topic10_Popup_Jframe.java
+++ selenium/webdriver_api/Topic10_Popup_Jframe.java
@@ -1,5 +1,7 @@
 package webdriver_api;
+
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -15,78 +17,133 @@ public class Topic10_Popup_Jframe {
 	// Khai báo biến driver đại diện cho selenium webdriver
 	WebDriver driver;
 
-	//Pre-Condition
+	// Pre-Condition
 	@BeforeClass
 	public void beforeClass() {
 		// Khởi tạo trình duyệt firefox
 		System.setProperty("webdriver.chrome.driver", ".\\libraries\\chromedriver.exe");
 		driver = new ChromeDriver();
-		
+
 		// Chờ chơ element được hiển thị trước khi tương tác trong vòng 30s
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
+
 		// Phóng to trình duyệt
 		driver.manage().window().maximize();
-		
-	
+
 	}
 
+	// POP-UP_IFRAME
 	@Test
 	public void TC_01_Popup_Iframe() throws InterruptedException {
-		//Mở ra 1 trang web 
+		// Mở ra 1 trang web
 		driver.get("https://kyna.vn/");
 		Thread.sleep(5000);
-		//Steps 03: Kiểm tra nếu pop-up xuất hiện thì đóng pop-up
-		List <WebElement> fancypopup=  driver.findElements(By.xpath("//div[@class='fancybox-inner']"));
-		if(fancypopup.size()>0)
-		{
+		// Steps 03: Kiểm tra nếu pop-up xuất hiện thì đóng pop-up
+		List<WebElement> fancypopup = driver.findElements(By.xpath("//div[@class='fancybox-inner']"));
+		if (fancypopup.size() > 0) {
 			Assert.assertTrue(fancypopup.get(0).isDisplayed());
-			driver.findElement(By.cssSelector("")).click();
+			driver.findElement(By.xpath("//img[@class='fancybox-image']")).click();
 		}
-		
-	
-		//Steps04- nếu pop-up không hiển thị thì switch đến Iframe
-		//Switch vào iframe trước thì mới tương tác được với element trong iframe
-		//Theo index
-		//driver.switchTo().frame(1);
-		//Theo id/ name
-		//Theo Webelement
+
+		// Steps04- nếu pop-up không hiển thị thì switch đến Iframe
+		// Switch vào iframe trước thì mới tương tác được với element trong iframe
+		// Theo index
+		// driver.switchTo().frame(1);
+		// Theo id/ name
+		// Theo Webelement
 		driver.switchTo().frame(driver.findElement(By.xpath("//div[@class='fanpage']//iframe")));
-		boolean iframefacebook= driver.findElement(By.cssSelector("#facebook")).isDisplayed();
+		boolean iframefacebook = driver.findElement(By.cssSelector("#facebook")).isDisplayed();
 		Assert.assertTrue(iframefacebook);
-		 //Verify lượt like = 170 nghìn
-		String likes=driver.findElement(By.xpath("//a[text()='Kyna.vn']/parent::div/following-sibling::div")).getText();
+		// Verify lượt like = 170 nghìn
+		String likes = driver.findElement(By.xpath("//a[text()='Kyna.vn']/parent::div/following-sibling::div"))
+				.getText();
 		Assert.assertEquals(likes, "170K likes");
-		
-		//Đã switch vào iframe rồi thì phải chuyển về main page
+
+		// Đã switch vào iframe rồi thì phải chuyển về main page
 		driver.switchTo().defaultContent();
 		driver.findElement(By.xpath("//a[@class='button-login' and text()='Đăng nhập']")).click();
 		driver.findElement(By.xpath("//input[@id='user-login']")).sendKeys("automationfc.vn@gmail.com");
 		driver.findElement(By.xpath("//input[@id='user-password']")).sendKeys("automationfc.vn@gmail.com");
 		Thread.sleep(1000);
 		driver.findElement(By.xpath("//button[@id='btn-submit-login']")).click();
-		
-		
-		WebElement userLogin= driver.findElement(By.xpath("//li[@class='account dropdown wrap']//span[@class='user']"));
+
+		WebElement userLogin = driver
+				.findElement(By.xpath("//li[@class='account dropdown wrap']//span[@class='user']"));
 		Assert.assertTrue(userLogin.isDisplayed());
 		Thread.sleep(2000);
 		Assert.assertEquals(userLogin.getText(), "Automation FC");
-		
+
 	}
 
+	// WINDOW_TAB
+	//@Test
+	public void TC02_Window_Tab() {
+		driver.get("https://automationfc.github.io/basic-form/index.html");
+		// Trả ra ID của tab đang đứng hay tab đang active
+		String parentID = driver.getWindowHandle();
+		System.out.println("Parent ID=" + parentID);
 
-	@Test
-	public void TC_03_() {
-		
+		// Click link google
+		driver.findElement(By.xpath("//a[text()='GOOGLE']")).click();
+		// Trả ra ID của tất cả các tab
+
+		switchToWindowByID(parentID);
+		Assert.assertEquals(driver.getTitle(), "Google");
+
+		// Quay lại parent window
+		driver.switchTo().window(parentID);
+
+		// Click link facebook
+		driver.findElement(By.xpath("//a[text()='FACEBOOK']")).click();
+		// Trả ra ID của tất cả các tab
+
+		switchToWindowByTitle(parentID);
+		Assert.assertEquals(driver.getTitle(), "Facebook - Đăng nhập hoặc đăng ký");
+
 	}
 
-	//Pro-conditon
+	// Hàm chạy với 2 cửa sổ (nhiều hơn 2 cửa sổ sẽ false)
+	public void switchToWindowByID(String parentID) {
+		// Lấy ra tất cả các ID đang có
+		Set<String> allWindows = driver.getWindowHandles();
+		// Dùng vòng lặp để duyệt qua từng ID
+		for (String termID : allWindows) {
+			// Kiểm tra ID nào mà khác với parent ID thì switch qua Tab/Window
+			if (!termID.equals(parentID))
+
+			{
+
+				// switch qua tab/window đó
+				driver.switchTo().window(termID);
+
+				break;
+			}
+		}
+
+	}
+	// Hàm chạy với > 2 cửa sổ( đúng với mọi trường hợp)
+	public void switchToWindowByTitle(String expectTitle) {
+		// Lấy ra tất cả các ID đang có
+		Set<String> allWindows = driver.getWindowHandles();
+		// Dùng vòng lặp để duyệt qua từng ID
+		for (String termID : allWindows) {
+			// Switch vào window trước
+			driver.switchTo().window(termID);
+			// Get ra title đang có
+			String currentWindow = driver.getTitle();
+			// Kiểm tra title nào mà bắng với title mong muốn thì break khỏi vòng lắp
+			if (currentWindow.equals(expectTitle))
+				break;
+		
+		}
+
+	}
+
+	// Pro-conditon
 	@AfterClass
 	public void afterClass() {
-		//Tắt trình duyệt
+		// Tắt trình duyệt
 		driver.quit();
 	}
 
 }
-
-
--- selenium/webdriver_api/Topic11_Window_Tab.java
+++ selenium/webdriver_api/Topic11_Window_Tab.java
@@ -1,10 +1,14 @@
 package webdriver_api;
+
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -13,70 +17,155 @@ public class Topic11_Window_Tab {
 	// Khai báo biến driver đại diện cho selenium webdriver
 	WebDriver driver;
 
-	//Pre-Condition
+	// Pre-Condition
 	@BeforeClass
 	public void beforeClass() {
 		// Khởi tạo trình duyệt firefox
 		System.setProperty("webdriver.chrome.driver", ".\\libraries\\chromedriver.exe");
 		driver = new ChromeDriver();
-		
+
 		// Chờ chơ element được hiển thị trước khi tương tác trong vòng 30s
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		
+
 		// Phóng to trình duyệt
 		driver.manage().window().maximize();
-		
 
 	}
 
-	@Test
-	public void TC_01() {
+	// WINDOW_TAB
+	//@Test
+	public void TC01_Window_Tab() {
 		driver.get("https://automationfc.github.io/basic-form/index.html");
-		
-		//Lấy ra ID của window/ tab đang active
-		String parentID=driver.getWindowHandle();
-		System.out.println("Parent ID="+ parentID);
-		//Click link GG
-		driver.findElement(By.xpath("//div[@class='container']//a[text()='GOOGLE']")).click();
+		// Trả ra ID của tab đang đứng hay tab đang active
+		String parentID = driver.getWindowHandle();
+		System.out.println("Parent ID=" + parentID);
 
-		//Lấy ra ID của tất cả các window/ tab
-		Set <String> allIDs= driver.getWindowHandles();
-		for (String id: allIDs) {
-			System.out.println("ID="+ id);
-		}
-		//So sánh: Nếu cái nào mà khác parent thì switch qua trong trường hợp chỉ có 2 cửa sổ
-		
-		
+		// Click link google
+		driver.findElement(By.xpath("//a[text()='GOOGLE']")).click();
+		// Trả ra ID của tất cả các tab
+
+		switchToWindowByID(parentID);
+		Assert.assertEquals(driver.getTitle(), "Google");
+
+		// Quay lại parent window
+		driver.switchTo().window(parentID);
+
+		// Click link facebook
+		driver.findElement(By.xpath("//a[text()='FACEBOOK']")).click();
+		// Trả ra ID của tất cả các tab
+
+		switchToWindowByTitle(parentID);
+		Assert.assertEquals(driver.getTitle(), "Facebook - Đăng nhập hoặc đăng ký");
+
+		// Quay lại parent window
+		driver.switchTo().window(parentID);
+
+		// Click tiki, switch qua tab mới
+		driver.findElement(By.xpath("//a[text()='TIKI']")).click();
+		switchToWindowByTitle(parentID);
+		Assert.assertEquals(driver.getTitle(), "Mua Hàng Trực Tuyến Uy Tín với Giá Rẻ Hơn tại Tiki.vn");
+
+		closeAllwindowwithoutparent(parentID);
+		// Quay lại parent window
+		Assert.assertEquals(driver.getTitle(), "SELENIUM WEBDRIVER FORM DEMO");
+
 	}
-	//Theo ID
-	public void switchTWindowByID(String parentID) {
-		//Lấy ra tất cả ID đang có và lưu vào allWindows
-		Set <String> allWindows= driver.getWindowHandles();
-		//Dùng vòng lặp duyệt qua từng ID
-		for(String runWindow: allWindows) {
-			if(!runWindow.equals(parentID)) {
-				driver.switchTo().window(runWindow);
+
+	// Hàm chạy với 2 cửa sổ (nhiều hơn 2 cửa sổ sẽ false)
+	public void switchToWindowByID(String parentID) {
+		// Lấy ra tất cả các ID đang có
+		Set<String> allWindows = driver.getWindowHandles();
+		// Dùng vòng lặp để duyệt qua từng ID
+		for (String termID : allWindows) {
+			// Kiểm tra ID nào mà khác với parent ID thì switch qua Tab/Window
+			if (!termID.equals(parentID))
+
+			{
+
+				// switch qua tab/window đó
+				driver.switchTo().window(termID);
+
 				break;
 			}
 		}
+
 	}
-	@Test
-	public void TC_02_() {
-	
+
+	// Hàm chạy với > 2 cửa sổ( đúng với mọi trường hợp)
+	public void switchToWindowByTitle(String expectTitle) {
+		// Lấy ra tất cả các ID đang có
+		Set<String> allWindows = driver.getWindowHandles();
+		// Dùng vòng lặp để duyệt qua từng ID
+		for (String termID : allWindows) {
+			// Switch vào window trước
+			driver.switchTo().window(termID);
+			// Get ra title đang có
+			String currentWindow = driver.getTitle();
+			// Kiểm tra title nào mà bắng với title mong muốn thì break khỏi vòng lắp
+			if (currentWindow.equals(expectTitle))
+				break;
+
+		}
+
+	}
+
+	// Đóng tất cả các window/tab ngoại trừ parent
+
+	public boolean closeAllwindowwithoutparent(String parentID) {
+		// Lấy ra tất cả các ID đang có
+		Set<String> allWindows = driver.getWindowHandles();
+
+		// Dùng vòng lặp để duyệt qua từng ID
+		for (String runWindows : allWindows) {
+			// Nếu ID nào mà khác với parent thì switch qua
+			if (!runWindows.equals(parentID)) {
+				// switch vào window đó
+				driver.switchTo().window(runWindows);
+				// close tab/ window đang đứng
+				driver.close();
+			}
+		}
+		driver.switchTo().window(parentID);
+		if (driver.getWindowHandles().size() == 1)
+			return true;
+		else
+			return false;
+
 	}
 
 	@Test
-	public void TC_03_() {
-		
+	public void TC02_Window_Tab() throws InterruptedException {
+		driver.get("https://kyna.vn/");
+		Thread.sleep(5000);
+		// Steps 03: Kiểm tra nếu pop-up xuất hiện thì đóng pop-up
+		List<WebElement> fancypopup = driver.findElements(By.xpath("//div[@class='fancybox-inner']"));
+		if (fancypopup.size() > 0) {
+			Assert.assertTrue(fancypopup.get(0).isDisplayed());
+			driver.findElement(By.xpath("//img[@class='fancybox-image']")).click();
+		}
+
+		// Steps04- nếu pop-up không hiển thị thì switch đến Iframe
+		// Switch vào iframe trước thì mới tương tác được với element trong iframe
+		// Theo index
+		// driver.switchTo().frame(1);
+		// Theo id/ name
+		// Theo Webelement
+
+		// Trả ra ID của tab đang đứng hay tab đang active
+		String parentID = driver.getWindowHandle();
+		System.out.println("Parent ID=" + parentID);
+
+		driver.findElement(By.xpath("//div[@class='hotline']//img[@alt='facebook']")).click();
+		switchToWindowByTitle(parentID);
+		Assert.assertEquals(driver.getTitle(), "Kyna.vn - Trang chủ");
+
 	}
 
-	//Pro-conditon
-	@AfterClass
+	// Pro-conditon
+	// @AfterClass
 	public void afterClass() {
-		//Tắt trình duyệt
+		// Tắt trình duyệt
 		driver.quit();
 	}
 
 }
-
-
